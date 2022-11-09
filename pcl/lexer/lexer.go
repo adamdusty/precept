9af5541c2d5353cd7b8c7da6b26da088b7c335be
Adam Dusty <adam.dusty27@gmail.com>
2022-11-09 15:07:56 -0700

    Tokenize string and number literals

--- pcl/lexer/lexer.go
+++ pcl/lexer/lexer.go
@@ -2,9 +2,33 @@ package lexer
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 	"unicode/utf8"
 
 	"github.com/adamdusty/precept/pcl/token"
+)
+
+var (
+	id_start = []*unicode.RangeTable{
+		unicode.L,
+		unicode.Nl,
+		unicode.Other_ID_Start,
+	}
+	id_continue = []*unicode.RangeTable{
+		unicode.L,
+		unicode.Nl,
+		unicode.Other_ID_Start,
+		unicode.Mn,
+		unicode.Mc,
+		unicode.Nd,
+		unicode.Pc,
+		unicode.Other_ID_Continue,
+	}
+	id_exclude = []*unicode.RangeTable{
+		unicode.Pattern_Syntax,
+		unicode.Pattern_White_Space,
+	}
 )
 
 type lexerState struct {
@@ -16,31 +40,63 @@ type lexerState struct {
 	Error    string
 }
 
-func (l *lexerState) advance(n int) lexerState {
-	source := l.Source
-	length := l.Length
-	line := l.Line
-	col := l.Column + n
-	pos := l.Position + n
-	err := l.Error
-	return lexerState{Source: source, Line: line, Column: col, Position: pos, Length: length, Error: err}
+func (l *lexerState) advance(n int) *lexerState {
+	next := new(lexerState)
+	next.Source = l.Source
+	next.Length = l.Length
+	next.Line = l.Line
+	next.Column = l.Column + n
+	next.Position = l.Position + n
+	next.Error = l.Error
+	return next
+}
+
+func (lex *lexerState) match(c rune) bool {
+	if !(lex.Position < lex.Length-1) {
+		return false
+	}
+
+	next := lex.Source[lex.Position+1]
+
+	return next == c
+}
+
+func (lex *lexerState) peek() rune {
+	if !(lex.Position < lex.Length-1) {
+		return utf8.RuneError
+	}
+	return lex.Source[lex.Position+1]
+}
+
+func isIdentStartChar(c rune) bool {
+	return unicode.IsOneOf(id_start, c)
+}
+
+func isIdentContinueChar(c rune) bool {
+	return unicode.IsOneOf(id_continue, c) && !unicode.IsOneOf(id_exclude, c)
 }
 
 func Tokenize(source string) ([]token.Token, error) {
-	state := lexerState{Source: []rune(source), Line: 1, Column: 0, Position: 0, Length: utf8.RuneCountInString(source) - 1, Error: ""}
+	state := new(lexerState)
+	state.Source = []rune(source)
+	state.Line = 1
+	state.Column = 0
+	state.Position = 0
+	state.Length = utf8.RuneCountInString(source)
+	state.Error = ""
 	tokens := []token.Token{}
 
 	var tok token.Token
 
 	for {
-		tok, state = scanNextToken(&state)
+		tok, state = scanNextToken(state)
 		tokens = append(tokens, tok)
 
 		if tok.Type == token.Error {
 			state.Error = tok.Lexeme
 		}
 
-		if state.Position > state.Length {
+		if state.Position > state.Length-1 {
 			break
 		}
 	}
@@ -52,8 +108,12 @@ func Tokenize(source string) ([]token.Token, error) {
 	return tokens, nil
 }
 
-func scanNextToken(state *lexerState) (token.Token, lexerState) {
+func scanNextToken(state *lexerState) (token.Token, *lexerState) {
 	current := state.Source[state.Position]
+
+	if unicode.IsSpace(current) {
+		state = state.advance(1)
+	}
 
 	switch current {
 	case '(':
@@ -111,289 +171,75 @@ func scanNextToken(state *lexerState) (token.Token, lexerState) {
 			return token.Token{Type: token.Less, Line: state.Line, Column: state.Column}, state.advance(1)
 		}
 	case '"':
-		return token.Token{Type: token.Error, Lexeme: "Unimplemented."}, state.advance(1)
+		return readStringLiteral(state)
 	default:
+		if unicode.IsDigit(current) {
+			return readNumberLiteral(state)
+		}
+
+		if isIdentStartChar(current) {
+			return readIdentifier(state)
+		}
 		return token.Token{Type: token.Error, Lexeme: "Unimplemented."}, state.advance(1)
 	}
 }
 
-func (lex *lexerState) match(c rune) bool {
-	if !(lex.Position < lex.Length) {
-		return false
+func readStringLiteral(state *lexerState) (token.Token, *lexerState) {
+	builder := strings.Builder{}
+
+	for i := state.Position + 1; i <= state.Length-1; i += 1 {
+		c := state.Source[i]
+		if c == '"' {
+			break
+		}
+
+		builder.WriteRune(c)
 	}
 
-	next := lex.Source[lex.Position+1]
+	tok := token.Token{Type: token.String, Line: state.Line, Column: state.Column, Lexeme: builder.String()}
 
-	return next == c
+	// Advance state by 2 to account for the first '"' we didn't consume on entry
+	return tok, state.advance(utf8.RuneCountInString(tok.Lexeme) + 2)
 }
 
-func (lex *lexerState) peek() rune {
-	if !(lex.Position < lex.Length) {
-		return utf8.RuneError
+func readNumberLiteral(state *lexerState) (token.Token, *lexerState) {
+	pos := state.Position
+	builder := strings.Builder{}
+
+	for pos < state.Length {
+		if !unicode.IsDigit(state.Source[pos]) {
+			break
+		}
+
+		builder.WriteRune(state.Source[pos])
+
+		pos += 1
 	}
-	return lex.Source[lex.Position+1]
+
+	if pos >= state.Length {
+		tok := token.Token{Type: token.Number, Line: state.Line, Column: state.Column, Lexeme: builder.String()}
+		return tok, state.advance(utf8.RuneCountInString(tok.Lexeme) + 1)
+	}
+
+	if state.Source[pos] == '.' {
+		// Write decimal
+		builder.WriteRune(state.Source[pos])
+		pos += 1
+
+		for pos < state.Length {
+			if !unicode.IsDigit(state.Source[pos]) {
+				break
+			}
+			builder.WriteRune(state.Source[pos])
+
+			pos += 1
+		}
+	}
+
+	tok := token.Token{Type: token.Number, Line: state.Line, Column: state.Column, Lexeme: builder.String()}
+	return tok, state.advance(pos)
 }
 
-// func Tokenize(rd io.Reader) ([]token.Token, error) {
-// 	lex := lexer{source: *bufio.NewReader(rd), line: 1, column: 0}
+func readIdentifier(state *lexerState) (token.Token, *lexerState) {
 
-// 	var tokens []token.Token
-
-// 	for {
-// 		tok := scanNextToken(&lex)
-// 		if tok.Type == token.EOF {
-// 			break
-// 		}
-
-// 		if tok.Type == token.Error {
-// 			log.Printf("ERROR: %s", tok.Lexeme)
-// 			return []token.Token{}, errors.New(tok.Lexeme)
-// 		}
-
-// 		tokens = append(tokens, tok)
-// 	}
-
-// 	return tokens, nil
-// }
-
-// type lexer struct {
-// 	source bufio.Reader
-// 	line   int
-// 	column int
-// }
-
-// func scanNextToken(lex *lexer) token.Token {
-// 	for c, _, err := lex.source.ReadRune(); err != io.EOF; {
-// 		if c == '\n' {
-// 			lex.line += 1
-// 			continue
-// 		}
-
-// 		if unicode.IsSpace(c) {
-// 			continue
-// 		}
-
-// 		switch c {
-// 		case '(':
-// 			tok := token.Token{Type: token.LeftParen, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case ')':
-// 			tok := token.Token{Type: token.RightParen, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '{':
-// 			tok := token.Token{Type: token.LeftBrace, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '}':
-// 			tok := token.Token{Type: token.RightBrace, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '[':
-// 			tok := token.Token{Type: token.LeftBracket, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case ']':
-// 			tok := token.Token{Type: token.RightBracket, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '.':
-// 			tok := token.Token{Type: token.Dot, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case ',':
-// 			tok := token.Token{Type: token.Comma, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case ':':
-// 			tok := token.Token{Type: token.Colon, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case ';':
-// 			tok := token.Token{Type: token.SemiColon, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '?':
-// 			tok := token.Token{Type: token.QuestionMark, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '/':
-// 			tok := token.Token{Type: token.Slash, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '*':
-// 			tok := token.Token{Type: token.Star, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '+':
-// 			tok := token.Token{Type: token.Plus, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '-':
-// 			tok := token.Token{Type: token.Minus, Line: lex.line, Column: lex.column}
-// 			lex.column += 1
-// 			return tok
-// 		case '=':
-// 			if lex.match('=') {
-// 				tok := token.Token{Type: token.EqualEq, Line: lex.line, Column: lex.column}
-// 				lex.column += 2
-// 				return tok
-// 			} else {
-// 				tok := token.Token{Type: token.Equal, Line: lex.line, Column: lex.column}
-// 				lex.column += 1
-// 				return tok
-// 			}
-// 		case '!':
-// 			if lex.match('=') {
-// 				tok := token.Token{Type: token.BangEq, Line: lex.line, Column: lex.column}
-// 				lex.column += 2
-// 				return tok
-// 			} else {
-// 				tok := token.Token{Type: token.Bang, Line: lex.line, Column: lex.column}
-// 				lex.column += 1
-// 				return tok
-// 			}
-// 		case '>':
-// 			if lex.match('=') {
-// 				tok := token.Token{Type: token.GreaterEq, Line: lex.line, Column: lex.column}
-// 				lex.column += 2
-// 				return tok
-// 			} else {
-// 				tok := token.Token{Type: token.Greater, Line: lex.line, Column: lex.column}
-// 				lex.column += 1
-// 				return tok
-// 			}
-// 		case '<':
-// 			if lex.match('=') {
-// 				tok := token.Token{Type: token.LessEq, Line: lex.line, Column: lex.column}
-// 				lex.column += 2
-// 				return tok
-// 			} else {
-// 				tok := token.Token{Type: token.Less, Line: lex.line, Column: lex.column}
-// 				lex.column += 1
-// 				return tok
-// 			}
-// 		case '"':
-// 			tok := lex.readStringLiteral()
-// 			lex.column += utf8.RuneCountInString(tok.Lexeme)
-// 			return tok
-// 		default:
-// 			if unicode.IsDigit(c) {
-// 				tok := lex.readNumberLiteral()
-// 				lex.column += utf8.RuneCountInString(tok.Lexeme)
-// 				return tok
-// 			}
-// 			return token.Token{Type: token.Error, Lexeme: "Unrecognized character."}
-// 		}
-// 	}
-
-// 	return token.Token{Type: token.EOF}
-// }
-
-// func (lex *lexer) match(c rune) bool {
-// 	next, _, err := lex.source.ReadRune()
-// 	if err != nil {
-// 		return false
-// 	}
-
-// 	if next == c {
-// 		return true
-// 	}
-
-// 	lex.source.UnreadRune()
-// 	return false
-// }
-
-// func (lex *lexer) peek() rune {
-// 	c, _, err := lex.source.ReadRune()
-
-// 	if err != nil {
-// 		return unicode.ReplacementChar
-// 	}
-
-// 	lex.source.UnreadRune()
-// 	return c
-// }
-
-// func (lex *lexer) advance() rune {
-// 	c, _, err := lex.source.ReadRune()
-
-// 	if err != nil {
-// 		return unicode.ReplacementChar
-// 	}
-
-// 	return c
-// }
-
-// func (lex *lexer) readStringLiteral() token.Token {
-// 	var builder strings.Builder
-
-// 	for {
-// 		c, _, err := lex.source.ReadRune()
-// 		if err != nil {
-// 			if err == io.EOF {
-// 				return token.Token{Type: token.Error, Lexeme: "Expected closing \""}
-// 			}
-
-// 			return token.Token{Type: token.Error, Lexeme: "Error while scanning string literal"}
-// 		}
-
-// 		if c == '"' {
-// 			break
-// 		}
-
-// 		builder.WriteRune(c)
-// 	}
-
-// 	tok := token.Token{Type: token.String, Line: lex.line, Column: lex.column, Lexeme: builder.String()}
-
-// 	return tok
-// }
-
-// func (lex *lexer) readNumberLiteral() token.Token {
-// 	lex.source.UnreadRune()
-// 	var builder strings.Builder
-
-// 	for {
-// 		c, _, err := lex.source.ReadRune()
-// 		if err != nil {
-// 			if err == io.EOF {
-// 				return token.Token{Type: token.Number, Line: lex.line, Column: lex.column, Lexeme: builder.String()}
-// 			}
-
-// 			return token.Token{Type: token.Error, Lexeme: "Error while scanning string literal", Line: lex.line, Column: lex.column}
-// 		}
-
-// 		if unicode.IsDigit(c) {
-// 			builder.WriteRune(c)
-// 		} else {
-// 			lex.source.UnreadRune()
-// 			break
-// 		}
-// 	}
-
-// 	if c := lex.peek(); c == '.' {
-// 		builder.WriteRune(lex.advance())
-
-// 		for {
-// 			c, _, err := lex.source.ReadRune()
-// 			if err != nil {
-// 				if err == io.EOF {
-// 					return token.Token{Type: token.Number, Line: lex.line, Column: lex.column, Lexeme: builder.String()}
-// 				}
-
-// 				return token.Token{Type: token.Error, Lexeme: "Error while scanning string literal", Line: lex.line, Column: lex.column}
-// 			}
-
-// 			if unicode.IsDigit(c) {
-// 				builder.WriteRune(c)
-// 			} else {
-// 				lex.source.UnreadRune()
-// 				break
-// 			}
-// 		}
-// 	}
-
-// 	return token.Token{Type: token.Number, Line: lex.line, Column: lex.column, Lexeme: builder.String()}
-// }
+}
